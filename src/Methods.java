09941024b35a24a31f35da6f2b33030226e64728
UnknownRebell <46301150+UnknownRebell@users.noreply.github.com>
2021-12-23 13:25:02 +0100

    Add files via upload

---
+++ src/Methods.java
@@ -0,0 +1,14 @@
+public class Methods {
+
+    public static int huh = "huh????";
+
+    public static void ente() {
+
+        if(huh.equals("huh?")) {
+            System.out.println(HUH????);
+        }
+
+
+
+
+}
